9cf3b6fb3df9c82e96073e90318042e9ac547d19
Malhar J. Mane <malharmanekk@gmail.com>
2025-05-04 12:40:39 -0700

    second commit

--- src/parts/BrandIcon.js
+++ src/parts/BrandIcon.js
@@ -2,20 +2,20 @@
 /* eslint-disable react/jsx-filename-extension */
 /* eslint-disable import/extensions */
 
-import React from 'react';
-
-import Button from '../elements/Button';
+import React from "react";
+import Button from "../elements/Button";
+import logo from "../assets/images/logo.png";
 
 export default function BrandIcon() {
   return (
-    <Button
-      className=""
-      type="link"
-      href="/"
-    >
-      <p className="text-theme-blue text-4xl font-medium ">
-        Racx
-        <span className="text-theme-purple">studio</span>
+    <Button type="link" href="/">
+      <img
+        src={logo}
+        alt="logo"
+        style={{ height: "80px", width: "auto", marginRight: "10px" }}
+      />
+      <p className="text-theme-blue text-4xl font-medium">
+        IRL<span className="text-theme-purple">Softwares</span>
       </p>
     </Button>
   );
